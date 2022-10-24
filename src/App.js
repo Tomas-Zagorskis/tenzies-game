b4a7da277264ea785e0e2eb0f8ff74c2679c5ef1
Tomas Zagorskis <tomzagos@gmail.com>
2022-10-24 20:49:45 +0300

    feat: implement win a game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import { nanoid } from 'nanoid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import ReactConfetti from 'react-confetti';
 import Die from './components/Die';
 
 export default function App() {
 	const [dice, setDice] = useState(allNewDice());
+
+	const [tenzie, setTenzie] = useState({
+		won: false,
+		moves: 0,
+	});
+
+	useEffect(() => {
+		if (dice.every(die => die.isHeld && die.value === dice[0].value)) {
+			setTenzie(prev => ({ ...prev, won: true }));
+		} else {
+			setTenzie(prev => ({ ...prev, won: false }));
+		}
+	}, [dice]);
 
 	function generateNewDie() {
 		return {
@@ -37,21 +51,38 @@ export default function App() {
 	}
 
 	function rollDice() {
-		setDice(oldDice =>
-			oldDice.map(die => (die.isHeld ? die : generateNewDie()))
-		);
+		if (!tenzie.won) {
+			setDice(oldDice =>
+				oldDice.map(die => (die.isHeld ? die : generateNewDie()))
+			);
+			setTenzie(prev => ({ ...prev, moves: prev.moves + 1 }));
+		} else {
+			setDice(allNewDice());
+			setTenzie({ won: false, moves: 0 });
+		}
 	}
+
+	const winMsg = (
+		<div className='modal'>
+			<h3 className='win-title'>Congratulation!!</h3>
+			<p className='win-message'>You won in {tenzie.moves} moves!</p>
+		</div>
+	);
 	return (
-		<main>
-			<h1 className='title'>Tenzies</h1>
-			<p className='instructions'>
-				Roll until all dice are the same. Click each die to freeze it at its
-				current value between rolls.
-			</p>
-			<div className='dice-container'>{diceElements}</div>
-			<button onClick={rollDice} className='roll-btn'>
-				Roll
-			</button>
-		</main>
+		<>
+			{tenzie.won && <ReactConfetti />}
+			<main>
+				{tenzie.won && winMsg}
+				<h1 className='title'>Tenzies</h1>
+				<p className='instructions'>
+					Roll until all dice are the same. Click each die to freeze it at its
+					current value between rolls.
+				</p>
+				<div className='dice-container'>{diceElements}</div>
+				<button onClick={rollDice} className='roll-btn'>
+					{tenzie.won ? 'New Game' : 'Roll'}
+				</button>
+			</main>
+		</>
 	);
 }
