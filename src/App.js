4913da2d92b7cc7f91f7cb2868c2bb34a9b53308
Tomas Zagorskis <tomzagos@gmail.com>
2022-10-24 17:42:52 +0300

    feat: add random dices

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import { useState } from 'react';
+import Die from './components/Die';
+
 export default function App() {
-	return <main></main>;
+	const [diceArray, setDiceArray] = useState(allNewDice());
+
+	function allNewDice() {
+		return Array(10)
+			.fill('')
+			.map((die, index) => {
+				const randomDie = Math.ceil(Math.random() * 6);
+				return <Die value={randomDie} key={index} />;
+			});
+	}
+	return (
+		<main>
+			<div className='dice-container'>{diceArray}</div>
+		</main>
+	);
 }
